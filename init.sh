9fa5f393facc46bbfba1c614e69b676ffbbf771f
DisposedCheese <assasin665@hotmail.com>
2013-07-19 15:51:41 -0400

    Added python check code

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@
 
 #Global Variables
 BERRYPIDIR=""
+PYTHONDIR= "/local/bin/python2.7"
 SOURCE=""
 DIR=""
 #
@@ -64,11 +65,66 @@ get_berrypi_path()
 		done	
 	fi
 }
+
+get_python_path()
+{
+	pyexist=false
+	#linux command type
+	# >/dev/null 2>&1 to mask the string that type echos 
+	type python2.7 >/dev/null 2>&1 || { local pyexist=false; echo >&2 "I require python 2.7 but it's not installed.";}
+	
+	if [ $pyexist == true ]; then
+		loopvar=1
+		if [ -f "/usr/bin/python2.7" ]; then
+			echo "Python 2.7 directory found"
+			echo "Continuing..."
+		else
+			while [ $loopvar == 1 ]; do 
+				echo "Enter the directory where python2.7 resides: "
+				read USERPATH
+	
+				PYTHONDIR="$USERPATH/python2.7"
+				if [ -f $PYTHONDIR ]; then 
+					echo "Python 2.7 directory provided is correct" 
+					echo "Continuing..."
+					loopvar=0 
+				else
+					echo "Python 2.7 directory specified is incorrect"
+					echo "Format(/subdir/pythondir) ex: /home/user/Desktop"
+		
+				fi
+			done	
+		fi
+	else
+		echo "Would you like to attempt to install Python 2.7?"
+		
+		read -p "(Y/n)?" choice
+  		case "$choice" in
+  		y|Y) 
+			sudo apt-get install python2.7 #May not work on all linux distros but hopefully all debian distros
+			if [ -f "/usr/bin/python2.7" ]; then
+				echo "Python 2.7 directory found"
+				echo "Continuing..."
+			else
+				echo "Python 2.7 directory not found"
+				#TODO allow user to look and provide path if their system installed python else where
+			fi
+		else	
+		;;
+		n|N)
+			echo "BerryPI initialization can not continue"
+			echo "Aborted"
+			exit 1	
+		;;
+		esac
+	fi 
+}
+
 #
 
 #Main
 get_berrypi_path
-
+get_python_path
 
 
 
