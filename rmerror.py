690442d523f0548a4d6f5550c3a356fca88bcc87
Oleg Rubel <rubel@gra-login1.graham.sharcnet>
2020-09-08 18:48:30 -0400

    improve error handling when WIEN2k fails

---
+++ rmerror.py
@@ -0,0 +1,17 @@
+'''
+Remove error files "*corename*.error" in the current directory.
+'''
+
+from __future__ import print_function # Python 2 & 3 compatible print function
+import os
+import sys
+import glob # pathnames matching a specified pattern
+
+import config
+from config import BERRY_DEFAULT_CONSOLE_PREFIX as DEFAULT_PREFIX
+
+def rmerror(corename):
+    pattern = '*'+corename+'*.error'
+    print(DEFAULT_PREFIX, "Cleaning error files:", pattern)
+    for errfilename in glob.glob(pattern):
+        os.remove(errfilename)
