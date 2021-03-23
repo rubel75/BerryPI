f0c3366c990dcb52dda271391e46d153c24237b0
himanshusaini11 <sainih5@mcmaster.ca>
2021-03-23 13:25:19 -0400

    Make a seperate solve() function for main calculation.

--- WloopPHI.py
+++ WloopPHI.py
@@ -10,7 +10,6 @@ import subprocess
 import os
 import sys
 import argparse # parse line arguments
-#import FileFormat, ReadInput
 
 def printEpilog():
     print('''
@@ -95,206 +94,219 @@ def ReadInputValues(content, WloopFileName):
         
     return (WorkingDir, KlistFileName, n, multiplier, StartBand, EndBand, S, E)
 
-
-
-try:
-    import numpy as np
-    print ("[OK] Numpy found")
-except ImportError as error:
-    print (error.__class__.__name__+": "+error.message)
-    sys.exit()
-
-# Set up parser for line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("infile",\
-        help="input file name",\
-        type=str)
-parser.add_argument("-sp",\
-        help="spin polarized calculation",\
-        action="store_true")
-parser.add_argument("-orb",\
-        help="calculation with an additional orbit potential (e.g., LSDA+U)",\
-        action="store_true")
-args = parser.parse_args()
-
-# Assign line arguments parsed by "argparse"
-WloopFileName = args.infile # input file name
-print("Input file name:",WloopFileName)
-options = "" # start with empty options and fill it up
-if args.sp: # enable spin polarization
-    options = options + '-sp'
-if args.orb: # enable orbital potential
-    options = options + '-orb'
-print("Additional options:",options)
-
-# Read input file
-print("Checking existance of",WloopFileName)
-if not(os.path.isfile(WloopFileName)):
-    print("{} does not exist, cannot finish calculation".format(WloopFileName))
-    exit(2)
-else:
-    print('-- OK')
-f = open(WloopFileName, 'r')
-content = f.readlines()
-f.close()
-
-if ("END" in content[-1]):
-    #content = content[:-3]
-    print ("Success")
-    WorkingDir, KlistFileName, n, multiplier, S_Band, E_Band, K_Start, K_End = ReadInputValues(
-            content, WloopFileName)
-else:
-    FileFormatMessage()
-    
-if not os.path.exists(WorkingDir):
-    print ("Error: Working directory does not exist.")
-    print ("Please check your working directory and try again. Good Bye :)")
-    sys.exit()
-    
-K_Points = []
-for i,j in zip(K_Start, K_End):
-    xmin = i[0]
-    xmax = j[0]
-    ymin = i[1]
-    ymax = j[1]
-    zmin = i[2]
-    zmax = j[2]
-    
-    var_x = np.linspace(xmin, xmax, num=n)
-    var_y = np.linspace(ymin, ymax, num=n)
-    var_z = np.linspace(zmin, zmax, num=n)
-    
-    for x, y, z in zip(var_x, var_y, var_z):
-        Klist = np.array([[x, y, z]])
-        #print (Klist)
-        K_Points.append(Klist)
+def Solve (*args):
+    K_Points = []
+    for i,j in zip(K_Start, K_End):
+        xmin = i[0]
+        xmax = j[0]
+        ymin = i[1]
+        ymax = j[1]
+        zmin = i[2]
+        zmax = j[2]
         
-    #break
-K_Points = np.concatenate(K_Points, axis=0)
-
-if ((var_x[1] - var_x[0]) == 0 and (var_y[1] - var_y[0]) == 0):
-    x_axis = var_z
-    direction = 'z'
-elif ((var_y[1] - var_y[0]) == 0 and (var_z[1] - var_z[0]) == 0):
-    x_axis = var_x
-    direction = 'x'
-else:
-    x_axis = var_y
-    direction = 'y'
-
-Data = []
-loop = 0
-
-for i in range(0, n):
-    print ("Wilson Loop ---------------------  %i of %i" %(i+1, n))
-    temp = i
-    Klist = []
-    for k in range(0, len(K_Start)):
+        var_x = np.linspace(xmin, xmax, num=n)
+        var_y = np.linspace(ymin, ymax, num=n)
+        var_z = np.linspace(zmin, zmax, num=n)
+        
+        for x, y, z in zip(var_x, var_y, var_z):
+            Klist = np.array([[x, y, z]])
+            #print (Klist)
+            K_Points.append(Klist)
+            
+        #break
+    K_Points = np.concatenate(K_Points, axis=0)
+    
+    if ((var_x[1] - var_x[0]) == 0 and (var_y[1] - var_y[0]) == 0):
+        x_axis = var_z
+        direction = 'z'
+    elif ((var_y[1] - var_y[0]) == 0 and (var_z[1] - var_z[0]) == 0):
+        x_axis = var_x
+        direction = 'x'
+    else:
+        x_axis = var_y
+        direction = 'y'
+    
+    Data = []
+    loop = 0
+    
+    for i in range(0, n):
+        print ("Wilson Loop ---------------------  %i of %i" %(i+1, n))
+        temp = i
+        Klist = []
+        for k in range(0, len(K_Start)):
+            #print (temp)
+            Klist.append(K_Points[temp])
+            temp = temp + n    
+        #temp = np.concatenate(Klist, axis=0)
+        #break
+        Klist = np.array(Klist)
+        size = np.size(Klist, 0)
+        #break
+        Klist = Klist * multiplier
+        Klist = np.c_[Klist, multiplier*np.ones(size), 2.00*np.ones(size)]
+        Klist = np.int_(Klist)
+        #break
+        filename = str("Wilson.klist")
+        np.savetxt(filename, Klist, fmt="          %5i%5i%5i%5i%5.1f", delimiter='', footer='END', comments='')
+        #print(Klist)
+        pwd = os.getcwd()
+        os.chdir(WorkingDir)
+        subprocess.call("mv %s/%s %s/%s" %(pwd, filename, WorkingDir, KlistFileName), shell = True)
+        subprocess.call("python /media/enigma/7167917D68894F96/GitHub/BerryPI/berrypi -so -w -b %i %i %s > std.out"%(S_Band, E_Band, options), shell=True)
+        berrypiOutFileName = str("%s.outputberry" %(str(WorkingDir.split('/')[-1])))
+        with open(berrypiOutFileName, 'r') as read_file:
+            for line in read_file:
+                if "Berry phase sum (rad) =" in line:
+                    #return line
+                    content = line
+                    #print (content)
+    
+        os.chdir(pwd)
+        #sys.exit()
+        
+        temp = float(content.split()[-1])
         #print (temp)
-        Klist.append(K_Points[temp])
-        temp = temp + n    
-    #temp = np.concatenate(Klist, axis=0)
-    #break
-    Klist = np.array(Klist)
-    size = np.size(Klist, 0)
-    #break
-    Klist = Klist * multiplier
-    Klist = np.c_[Klist, multiplier*np.ones(size), 2.00*np.ones(size)]
-    Klist = np.int_(Klist)
-    #break
-    filename = str("Wilson.klist")
-    np.savetxt(filename, Klist, fmt="          %5i%5i%5i%5i%5.1f", delimiter='', footer='END', comments='')
-    #print(Klist)
-    pwd = os.getcwd()
-    os.chdir(WorkingDir)
-    subprocess.call("mv %s/%s %s/%s" %(pwd, filename, WorkingDir, KlistFileName), shell = True)
-    subprocess.call("python $WIENROOT/SRC_BerryPI/BerryPI/berrypi -so -w -b %i %i %s"%(S_Band, E_Band, options), shell=True)
-    berrypiOutFileName = str("%s.outputberry" %(WorkingDir.split('/')[-1]))
-    with open(berrypiOutFileName, 'r') as read_file:
-        for line in read_file:
-            if "Berry phase sum (rad) =" in line:
-                #return line
-                content = line
-                #print (content)
-
-    os.chdir(pwd)
-    #sys.exit()
+        #sys.exit()
     
-    temp = float(content.split()[-1])
-    #print (temp)
-    #sys.exit()
-
-    temp1 = ((temp + np.pi) % (2 * np.pi) - np.pi)  # 2 pi wraping
-    #temp = np.array([i, temp])    
-    temp = np.array([x_axis[loop], temp, temp1])
-    loop += 1
+        temp1 = ((temp + np.pi) % (2 * np.pi) - np.pi)  # 2 pi wraping
+        #temp = np.array([i, temp])    
+        temp = np.array([x_axis[loop], temp, temp1])
+        loop += 1
+        
+        Data.append(temp)
+        
+    Data = np.array(Data)
     
-    Data.append(temp)
+    return (direction, Data)
+
+def Unwrap(Data):
+    BP = Data[:, 2]
+    BP_out = np.unwrap(BP, discont=float(1*np.pi), axis=-1)
+    diff = 0
+    Check_Diff = False
+    for i in BP_out:
+        diff = i-diff
+        if (diff > np.divide(np.pi, 2)):
+            Check_Diff = True
+        diff = i
     
-Data = np.array(Data)
+    BP_out = np.divide(BP_out, np.multiply(2, np.pi))
+    shape = BP_out.shape
+    BP_out.shape = (shape[0], 1)
+    
+    Data = np.append(Data, BP_out, axis=1)
+    
+    return (Check_Diff, Data)
+    
+    
+if __name__=="__main__":
+    import time
+    StartTime = time.time() # Calculation start time.
+    try:
+        import numpy as np
+        print ("[OK] Numpy found")
+    except ImportError as error:
+        print (error.__class__.__name__+": "+error.message)
+        sys.exit()
+    
+    # Set up parser for line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile",\
+            help="input file name",\
+            type=str)
+    parser.add_argument("-sp",\
+            help="spin polarized calculation",\
+            action="store_true")
+    parser.add_argument("-orb",\
+            help="calculation with an additional orbit potential (e.g., LSDA+U)",\
+            action="store_true")
+    args = parser.parse_args()
+    
+    # Assign line arguments parsed by "argparse"
+    WloopFileName = args.infile # input file name
+    print("Input file name: %s" %WloopFileName)
+    options = "" # start with empty options and fill it up
+    if args.sp: # enable spin polarization
+        options = options + '-sp'
+    if args.orb: # enable orbital potential
+        options = options + '-orb'
+    print("Additional options: %s" %options)
+    
+    # Read input file
+    print("Checking existance of %s" %WloopFileName)
+    if not(os.path.isfile(WloopFileName)):
+        print("{} does not exist, cannot finish calculation.".format(WloopFileName))
+        exit(2)
+    else:
+        print('-- OK')
+    f = open(WloopFileName, 'r')
+    content = f.readlines()
+    f.close()
+    
+    if ("END" in content[-1]):
+        #content = content[:-3]
+        print ("Success")
+        WorkingDir, KlistFileName, n, multiplier, \
+            S_Band, E_Band, K_Start, K_End = \
+                ReadInputValues(content, WloopFileName)
+    else:
+        FileFormatMessage()
+        
+    if not os.path.exists(WorkingDir):
+        print ("Error: Working directory does not exist.")
+        print ("Please check your working directory and try again. Thank you!")
+        sys.exit()
+        
+    ######################## Solver ############################################
+    direction, Data = Solve(WorkingDir, KlistFileName, n, multiplier, S_Band, E_Band, K_Start, K_End)
 
-######################### Unwrap ###########################################
+    ######################### Unwrap ###########################################
+    Check_Diff, Data = Unwrap(Data) 
 
-BP = Data[:, 2]
-BP_out = np.unwrap(BP, discont=3.141592653589793, axis=-1)
-
-diff = 0
-Check_Diff = False
-for i in BP_out:
-    diff = i-diff
-    if (diff > np.divide(np.pi, 2)):
-        Check_Diff = True
-    diff = i
-
-BP_out = np.divide(BP_out, np.multiply(2, np.pi))
-shape = BP_out.shape
-BP_out.shape = (shape[0], 1)
-
-Data = np.append(Data, BP_out, axis=1)
-
-#################### Save data to file #####################################
-
-outfile = str("PHI.dat")
-np.savetxt(outfile, Data, fmt='%5.5f', delimiter='          ', 
-           header='Loop (%s)       BerryPhase(BP)   BP(-/+pi wrap)   BP(unwrap)' %direction)
-
-print ("Calculation done!!!")
-if (Check_Diff == True):
-    print("WARNING -----> Phase Difference: The jump in phase difference is greter than pi/2 which is not good.")
-    print("To avoid this warning please increase the number of Wilson loops.")
-print ("Output data file ""PHI.dat"" has generated.")
-   
-#################### PLOT ##################################################
-try:
-    import matplotlib
-except ImportError as error: # matplotlib is not installed
-    print ("It seems that matplotlib is not installed, but it is not essential.")
-    print ("You can plot the figure yourself by using ""PHI.dat"" file.")
-    print ("Good bye!!! :)")
+    #################### Save data to file #####################################
+    outfile = str("PHI.dat")
+    np.savetxt(outfile, Data, fmt='%5.5f', delimiter='          ', 
+               header='Loop (%s)       BerryPhase(BP)   BP(-/+pi wrap)   BP(unwrap)' %direction)
+    print ("Calculation done!!!")
+    if (Check_Diff == True):
+        print("WARNING -----> Phase Difference: The jump in phase difference is greter than pi/2 which is not good.")
+        print("To avoid this warning please increase the number of Wilson loops.")
+    print ("Output data file ""PHI.dat"" has generated.")
+       
+    #################### PLOT ##################################################
+    try:
+        import matplotlib
+    except ImportError as error: # matplotlib is not installed
+        print (error.__class__.__name__+": "+error.message)    
+        print ("It seems that matplotlib is not installed, but it is not essential.")
+        print ("You can plot the figure yourself by using ""PHI.dat"" file.")
+        print ("Thank you!")
+        printEpilog()
+        sys.exit(0)
+    
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    print ("[OK] Matplotlib found")
+    fig, ax = plt.subplots(figsize=(6,6), dpi=300)
+    ax.plot(Data[:, 0], Data[:, 1], 'bo')
+    ax.set(xlabel = 'Loop in %s direction' %direction, ylabel = 'Berry Phase (radians)', title = 'Berry Phase Vs Wilson loop')
+    #ax.grid()
+    fig.savefig("BW_RawData.png")
+    #plt.show()
+    print ("Output figure ""BW_RawData.png"" has generated.")
+    
+    fig, ax = plt.subplots(figsize=(6,6), dpi=300)    
+    ax.plot(Data[:, 0], Data[:, 2], 'bo')
+    ax.set(xlabel = 'Loop in %s direction' %direction, ylabel = 'Berry Phase (radians)', title = 'Berry Phase Vs Wilson loop')
+    fig.savefig("BW_PiWrapped.png")
+    print ("Output figure ""BW_PiWrapped.png"" has been generated.")
+    
+    fig, ax = plt.subplots(figsize=(6,6), dpi=300)
+    ax.plot(Data[:, 0], Data[:, 3], 'bo')
+    ax.set(xlabel = 'Loop in %s direction' %direction, ylabel = 'Berry Phase (1/2pi)', title = 'Berry Phase Vs Wilson loop')
+    fig.savefig("BW_PhaseChange.png")
+    print ("Output figure ""BW_PhaseChange.png"" has been generated")
+    print ("Thank you!")
+    EndTime = time.time() # Calculation end time.
+    print ("Total time = %s" %time.strftime('%H:%M:%S', time.gmtime(float(EndTime - StartTime))))
     printEpilog()
-    sys.exit(0)
-
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-print ("[OK] Matplotlib found")
-fig, ax = plt.subplots(figsize=(6,6), dpi=300)
-ax.plot(Data[:, 0], Data[:, 1], 'bo')
-ax.set(xlabel = 'Loop in %s direction' %direction, ylabel = 'Berry Phase (radians)', title = 'Berry Phase Vs Wilson loop')
-#ax.grid()
-fig.savefig("BW_RawData.png")
-#plt.show()
-print ("Output figure ""BW_RawData.png"" has generated.")
-
-fig, ax = plt.subplots(figsize=(6,6), dpi=300)    
-ax.plot(Data[:, 0], Data[:, 2], 'bo')
-ax.set(xlabel = 'Loop in %s direction' %direction, ylabel = 'Berry Phase (radians)', title = 'Berry Phase Vs Wilson loop')
-fig.savefig("BW_PiWrapped.png")
-print ("Output figure ""BW_PiWrapped.png"" has been generated.")
-
-fig, ax = plt.subplots(figsize=(6,6), dpi=300)
-ax.plot(Data[:, 0], Data[:, 3], 'bo')
-ax.set(xlabel = 'Loop in %s direction' %direction, ylabel = 'Berry Phase (1/2pi)', title = 'Berry Phase Vs Wilson loop')
-fig.savefig("BW_PhaseChange.png")
-print ("Output figure ""BW_PhaseChange.png"" has been generated")
-print ("Good Bye!!! :)")
-printEpilog()
